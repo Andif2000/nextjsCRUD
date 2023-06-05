4f087c1d9daa5ca777d00386f3d737f28a029910
Andi Fahmi <andifahmi266@gmail.com>
2023-06-06 07:33:57 +0800

    Update Add product

--- app/product/addProduct.tsx
+++ app/product/addProduct.tsx
@@ -1,13 +1,35 @@
 'use client'
-import { useState } from 'react'
+import { useState, SyntheticEvent } from 'react'
 import type { Brand } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 const AddProduct = ({ brands }: { brands: Brand[] }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+  const [brand, setBrand] = useState('')
+
+  const router = useRouter()
 
   const handleModal = () => {
     setIsOpen(!isOpen)
   }
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault()
+    await axios.post('/api/product', {
+      title: title,
+      price: Number(price),
+      brandId: Number(brand),
+    })
+    setTitle('')
+    setPrice('')
+    setBrand('')
+    router.refresh()
+    setIsOpen(false)
+  }
+
   return (
     <div>
       <button className="btn bg-green-500 text-white" onClick={handleModal}>
@@ -16,13 +38,17 @@ const AddProduct = ({ brands }: { brands: Brand[] }) => {
       <div className={isOpen ? 'modal modal-open' : 'modal'}>
         <div className="modal-box">
           <h3 className="font-bold text-large">Add New Product</h3>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-control w-full">
               <label className="label font-bold">Product Name</label>
               <input
                 type="text"
                 className="input input-bordered"
                 placeholder="Product Name"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value)
+                }}
               />
             </div>
             <div className="form-control w-full">
@@ -31,11 +57,21 @@ const AddProduct = ({ brands }: { brands: Brand[] }) => {
                 type="text"
                 className="input input-bordered"
                 placeholder="Product Price"
+                value={price}
+                onChange={(e) => {
+                  setPrice(e.target.value)
+                }}
               />
             </div>
             <div className="form-control w-full">
               <label className="label font-bold">Product Brand</label>
-              <select className="select select-bordered">
+              <select
+                value={brand}
+                onChange={(e) => {
+                  setBrand(e.target.value)
+                }}
+                className="select select-bordered"
+              >
                 <option value="" disabled>
                   Select a Brand
                 </option>
