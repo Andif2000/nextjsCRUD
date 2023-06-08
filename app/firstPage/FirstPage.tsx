70c472bcc2b4bd28f83bcdc4fa54494cc627888a
Andi Fahmi <andifahmi266@gmail.com>
2023-06-08 09:35:09 +0800

    Create First Page

---
+++ app/firstPage/FirstPage.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Navbar from '../components/Navbar'
+
+const FirstPage = () => {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  )
+}
+
+export default FirstPage
