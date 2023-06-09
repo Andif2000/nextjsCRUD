f003c4a64b5f5ee8ea66b8a72025579da2b23d56
Andi Fahmi <andifahmi266@gmail.com>
2023-06-09 09:44:38 +0800

    Update FirstPage

--- app/firstPage/FirstPage.tsx
+++ app/firstPage/FirstPage.tsx
@@ -1,10 +1,18 @@
 'use client'
+import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Background from '../../public/images/background.jpg'
 
 const FirstPage = () => {
   return (
-    <div className="">
-      <Navbar />
+    <div className="relative h-screen">
+      <div className="flex h-full w-full">
+        <Image
+        src={Background}
+        className='w-full h-screen'
+        alt=''/>
+        <Navbar />
+      </div>
     </div>
   )
 }
