d110753638824636cd00a39695e43370dc3119de
Andi Fahmi <andifahmi266@gmail.com>
2023-06-09 20:26:30 +0800

    Update First Page

--- app/firstPage/FirstPage.tsx
+++ app/firstPage/FirstPage.tsx
@@ -5,13 +5,30 @@ import Background from '../../public/images/background.jpg'
 
 const FirstPage = () => {
   return (
-    <div className="relative h-screen">
-      <div className="flex h-full w-full">
-        <Image
-        src={Background}
-        className='w-full h-screen'
-        alt=''/>
+    <div
+      style={{
+        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/e/e0/Grass_at_a_lawn_with_morning_dew_02.jpg")`,
+        height: '100vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+      className="relative h-screen"
+    >
+      <div className="flex h-full w-full flex-col">
+        {/* <Image src={Background} className="w-full h-screen" alt="" /> */}
         <Navbar />
+        <div className="flex flex-row px-4 py-4">
+          <div className="flex font-bold text-4xl text-zinc-300 w-full justify-center">
+            Welcome to Andif20 Cell
+          </div>
+          <div className="flex w-full items-center justify-center">
+            <div className="flex bg-zinc-400">
+              <div className="flex w-96 justify-center">
+                <div className="flex font-bold text-4xl text-white">Login</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
